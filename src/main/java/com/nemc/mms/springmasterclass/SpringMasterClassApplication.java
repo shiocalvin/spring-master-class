a565498b8b1ee18a83b5c68ba04062196de35f92
calvinshio <calvinjshio@gmail.com>
2026-04-14 21:52:51 +0300

    feat --> first-commit

---
+++ src/main/java/com/nemc/mms/springmasterclass/SpringMasterClassApplication.java
@@ -0,0 +1,13 @@
+package com.nemc.mms.springmasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMasterClassApplication.class, args);
+    }
+
+}
